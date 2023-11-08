22959094c5e3c61414ae5f0d6d98507fa5eec7ef
AbhinavOhri <abhinavohri13@gmail.com>
2023-11-08 17:05:06 +0530

    Included the addition form

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
+def home():
+    return render_template("index.html")
+
+
+@app.route("/counter")
+def counter():
     return render_template("layout.html")
+
+
+@app.route("/form", methods=["GET", "POST"])
+def form():
+    if request.method == "POST":
+        num1 = request.form["num1"]
+        num2 = request.form["num2"]
+        sum = int(num1) + int(num2)
+        return render_template("output.html", sum=sum, num1=num1, num2=num2)
+    return render_template("form.html")
